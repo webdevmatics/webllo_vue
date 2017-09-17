ea80ca9004c2d3830d03b5843c568473fa2900f9
webdevmatics <webdevmatics@gmail.com>
2017-09-17 18:54:08 +0545

    dragg drop

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -1,33 +1,28 @@
 <template>
 	
-	<div >
+	<div>
 			<v-list class="grey lighten-3">
 
-			<draggable v-model="cards">
-			    <transition-group>
-			        <div v-for="card in cards" :key="card.id">
-			            {{card.name}}
-			        </div>
-			    </transition-group>
+			<draggable v-model="cards" :options="{group:'cards'}" @add="onAdd" style="min-height: 15px" :listId="list.id">
+			       <li v-for="card in cards" :key="card.id" :cardId="card.id">
+				        <v-list-tile avatar >
+				      		<v-list-tile-content>
+				              <v-list-tile-title
+				               style="border-radius:5px"
+				               class="white">
+				               		<div class="pl-3 py-2" >{{card.name}}</div>
+				               </v-list-tile-title>
+
+			            	</v-list-tile-content>
+				        </v-list-tile avatar>
+			        </li>
 			</draggable>
 
-
-
-	           <!-- <v-list-tile avatar v-for="card in cards" v-bind:key="card.id">
-		            <v-list-tile-content>
-		              <v-list-tile-title
-		               style="border-radius:5px"
-		               class="white">
-		               <div class="pl-3 py-2" >{{card.name}}</div>
-		                </v-list-tile-title>
-
-		            </v-list-tile-content>
-		          </v-list-tile>
-
-		          <v-list-tile @keyup.esc="editCardId=null">
+		 <v-list-tile @keyup.esc="editCardId=null">
 		          	<v-text-field @click.stop v-model="cardData.name" label="Card Name" v-if="list.id==editCardId" @keyup.enter="createCard(list.id)"></v-text-field>
 		          	<a @click="editCardId=list.id" v-else>Add card</a>
-		          </v-list-tile> -->
+		  </v-list-tile> 
+		         
 		 </v-list>
 	</div>
 
@@ -64,6 +59,26 @@ import draggable from 'vuedraggable'
 				this.cardData="";
 				this.editCardId="";
 			});
+		},
+		updateCard(cardId,listId){
+			axios.put('/card/'+cardId,{lists_id:listId})
+			.then(response=>{
+				console.log(response);
+			});
+		},
+		destroyCard(cardId){
+				axios.delete('/card/'+cardId)
+			.then(response=>{
+				console.log(response);
+			});
+		},
+		onAdd(evt){
+			console.log(evt);
+			let fromListId= evt.from.getAttribute('listId');
+			let cardId= evt.item.getAttribute('cardId');
+			let toListId= evt.to.getAttribute('listId');
+
+			this.updateCard(cardId,toListId);
 		}
 	}
 		
