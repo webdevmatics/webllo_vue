a08b34b35aa747d3f8f093f8b290be7b65acb75c
webdevmatics <webdevmatics@gmail.com>
2017-09-17 09:36:21 +0545

    cards create

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -2,7 +2,18 @@
 	
 	<div >
 			<v-list class="grey lighten-3">
-	           <v-list-tile avatar v-for="card in cards" v-bind:key="card.id">
+
+			<draggable v-model="cards">
+			    <transition-group>
+			        <div v-for="card in cards" :key="card.id">
+			            {{card.name}}
+			        </div>
+			    </transition-group>
+			</draggable>
+
+
+
+	           <!-- <v-list-tile avatar v-for="card in cards" v-bind:key="card.id">
 		            <v-list-tile-content>
 		              <v-list-tile-title
 		               style="border-radius:5px"
@@ -13,9 +24,10 @@
 		            </v-list-tile-content>
 		          </v-list-tile>
 
-		          <v-list-tile>
-		          	<a href="#">Add card</a>
-		          </v-list-tile>
+		          <v-list-tile @keyup.esc="editCardId=null">
+		          	<v-text-field @click.stop v-model="cardData.name" label="Card Name" v-if="list.id==editCardId" @keyup.enter="createCard(list.id)"></v-text-field>
+		          	<a @click="editCardId=list.id" v-else>Add card</a>
+		          </v-list-tile> -->
 		 </v-list>
 	</div>
 
@@ -23,17 +35,36 @@
 
 
 <script>
+import draggable from 'vuedraggable'
 	
 	export default {
 		props:['list'],
+		components:{draggable},
 		data() {
 			return { 
 				cards:'',
+				cardData:{name:''},
+				editCardId:''
 		}
 	},
 
 	created(){
 		this.cards=this.list.cards;
+	},
+
+	methods:{
+		createCard(listId) {
+			this.editCardId=listId;
+
+			axios.post('/boards/'+this.list.board_id+"/list/"+this.list.id+"/card",{name:this.cardData.name})
+			.then(response=>{
+				console.log(response);
+				this.cards.push(response.data.card);
+
+				this.cardData="";
+				this.editCardId="";
+			});
+		}
 	}
 		
 }
